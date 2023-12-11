dde455388bd175d68d481a1871487fe3720f44c6
icodingchef <icodingchef@gmail.com>
2023-12-11 19:53:39 +0900

    main.dart is revised

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+        colorSchemeSeed: Colors.blue,
+      ),
       home: const AnimalSounds(),
     );
   }
